251b95d9b36b576af272a554abcabdbfd787fac0
sagarkudu <sagar978@gmail.com>
2020-09-08 09:17:49 +0530

    commit

---
+++ build/precache-manifest.95abe6376ac81d6da80e69493e25d447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d316b9cd20ecd0c4a765c1e9c2b0b586",
+    "url": "/Fetch-mulitple-api-using-ReactJS/index.html"
+  },
+  {
+    "revision": "5b25cc39402769f28ef1",
+    "url": "/Fetch-mulitple-api-using-ReactJS/static/css/main.f5072505.chunk.css"
+  },
+  {
+    "revision": "2f63b720f1298a63daa8",
+    "url": "/Fetch-mulitple-api-using-ReactJS/static/js/2.7a987df7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Fetch-mulitple-api-using-ReactJS/static/js/2.7a987df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b25cc39402769f28ef1",
+    "url": "/Fetch-mulitple-api-using-ReactJS/static/js/main.bc8d0cc9.chunk.js"
+  },
+  {
+    "revision": "9e4b62278b21fd000d2e",
+    "url": "/Fetch-mulitple-api-using-ReactJS/static/js/runtime-main.068c8df2.js"
+  }
+]);
